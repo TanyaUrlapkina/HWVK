1dff980d54c5141c785cb3e292127b82858d0d10
TanyaUrlapkina <write2me92@mail.ru>
2021-12-12 00:14:57 +0300

    v2

--- src/main/java/ru/netology/domain/Donut.java
+++ src/main/java/ru/netology/domain/Donut.java
@@ -6,4 +6,30 @@ public class Donut {
     private boolean canPublishFreeCopy;
     private String editMode;
     private Placeholder placeholder;
+
+    public boolean isDonut() {
+        return isDonut;
+    }
+
+    public void setDonut(boolean donut) {
+        isDonut = donut;
+    }
+    public int getPaidDuration() {
+        return paidDuration;
+    }
+
+    public void setPaidDuration(int paidDuration) {
+        this.paidDuration = paidDuration;
+    }
+
+    public void setCanPublishFreeCopy(boolean canPublishFreeCopy) {
+        this.canPublishFreeCopy = canPublishFreeCopy;
+    }
+
+    public void setEditMode(String editMode) {
+        this.editMode = editMode;
+    }
+    public void setPlaceholder(Placeholder placeholder) {
+        this.placeholder = placeholder;
+    }
 }
