e084292ccf4922be28277cfe71a58c8bc29c556b
TanyaUrlapkina <write2me92@mail.ru>
2021-12-12 00:14:57 +0300

    v2

--- src/main/java/ru/netology/domain/CommentsInfo.java
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -4,4 +4,16 @@ public class CommentsInfo {
     private int count;
     private boolean usersComments;
     private boolean groupsComments;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setUsersComments(boolean usersComments) {
+        this.usersComments = usersComments;
+    }
+
+    public void setgroupsComments(boolean groupsComments) {
+        this.groupsComments = groupsComments;
+    }
 }
--- src/main/java/ru/netology/domain/LikesInfo.java
+++ src/main/java/ru/netology/domain/LikesInfo.java
@@ -4,4 +4,15 @@ public class LikesInfo {
     private  int count;
     private String icon;
 
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setIcon(String icon){
+        this.icon = icon;
+    }
 }
